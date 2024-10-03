f44cd9b4fc4713a88f71e0032c575af12e597911
Sihan Ma <masihan0303@gmail.com>
2024-10-03 10:52:46 +0100

    Delete a task

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -19,6 +19,12 @@ function App() {
     console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
 
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
+
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -29,6 +35,7 @@ function App() {
           deadline={task.deadline}
           key={task.id}
           markDone={() => doneHandler(index)}
+          deleteTask = {() => deleteHandler(index)}
         />
       ))}
     </div>
